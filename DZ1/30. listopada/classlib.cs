b2fbb51a2c4408dc2bba8ffc7190af30845006f7
Bruno Miletić <73548238+blajbaca@users.noreply.github.com>
2020-10-30 23:51:44 +0100

    Add files via upload

---
+++ DZ1/30. listopada/classlib.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Class_Library
+{
+    class Episode
+    {
+        private int numberOfViewers=0;
+        private double maxRating=0;
+        private double ratingSum=0;
+
+        public Episode() { }
+
+        public Episode(int numberOfViewers, double maxRating, double ratingSum)
+        {
+            this.numberOfViewers = numberOfViewers;
+            this.maxRating = maxRating;
+            this.ratingSum = ratingSum;
+
+        }
+        public void AddView(double number)
+        {
+            if (number > maxRating)
+            {
+                maxRating = number;
+                numberOfViewers += 1;
+            }
+        }
+        public static double GenerateRandomScore()
+        {
+            Random random = new Random();
+            return random.NextDouble() * 10;
+        }
+        public int GetViewerCount()
+        {
+            return numberOfViewers;
+        }
+        public double GetAverageScore()
+        {
+            return ratingSum / numberOfViewers;
+        }
+        public double GetMaxScore()
+        {
+            return maxRating;
+        }
+    }
+
+}
+
